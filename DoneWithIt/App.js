ce50c66ca86aa2456b54fce485f74569fd774bcd
Sandra-HN-Ali <sandra.hnaidy@ali-sons.com>
2022-01-06 17:03:33 +0400

    DoneWithIt

---
+++ DoneWithIt/App.js
@@ -0,0 +1,40 @@
+import { StatusBar as ExpoStatusBar } from "expo-status-bar";
+import {
+  SafeAreaView,
+  StyleSheet,
+  TouchableWithoutFeedback,
+  TouchableOpacity,
+  Text,
+  Image,
+  View,
+  Button,
+  Alert,
+  Platform,
+  StatusBar,
+  Dimensions,
+} from "react-native";
+
+import {
+  useDimensions,
+  useDeviceOrientation,
+} from "@react-native-community/hooks";
+import WelcomeScreen from "./app/screens/WelcomeScreen";
+import ViewImageScreen from "./app/screens/ViewImageScreen";
+
+export default function App() {
+  const { landscape } = useDeviceOrientation();
+
+  console.log(useDimensions(), useDeviceOrientation());
+  const handlePress = () => console.log("text clicked");
+  return <ViewImageScreen />;
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    paddingTop: Platform.OS === "android" ? StatusBar.currentHeight : 0,
+    // alignItems: "center",
+    // justifyContent: "center",
+  },
+});
